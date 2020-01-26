abda1649602dc98bbb92bada1da6a90016b971b0
Oskar Rainer <rainer.oskar@gmail.com>
2020-01-26 15:56:43 +0100

    set font and colors

--- src/globalStyles.ts
+++ src/globalStyles.ts
@@ -11,17 +11,21 @@ export const GlobalStyle = createGlobalStyle`
   }
 
   html {
-    font-size: calc(12px + 0.75vmin);
+    font-size: calc(10px + 0.75vmin);
     line-height: 1.5;
   }
 
   body {
-    font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+    background-color: #f5f9ff;
+    color: #333333;
+    font-family: 'Noto Sans', -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
     font-size: 1rem;
   }
 
   #root {
     height: 100%;
     position: relative;
+    overflow-y: scroll;
+    overflow-x: hidden;
   }
 `
