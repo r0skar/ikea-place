57198e5c7767b52c845b45f0406cc52e6138030c
Oskar Rainer <rainer.oskar@gmail.com>
2020-01-26 14:09:11 +0100

    add router transitions

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import React from 'react'
-import { Route, Switch } from 'react-router-dom'
+import styled from 'styled-components'
+import { Route, Switch, useLocation } from 'react-router-dom'
+import { AnimatePresence, motion } from 'framer-motion'
 import { useContent, Status } from './context/Content'
 import { CategoryList } from './views/CategoryList'
 import { ModelList } from './views/ModelList'
 import { ModelDetails } from './views/ModelDetails'
+
+const RouteContainer = styled(motion.div)`
+  position: absolute;
+  top: 0;
+  left: 0;
+  height: 100%;
+  width: 100%;
+`
 
 const Loader: React.FC = () => (
   <div>
@@ -17,19 +27,56 @@ const Error: React.FC = () => (
   </div>
 )
 
-const Router: React.FC = () => (
-  <Switch>
-    <Route exact path="/">
-      <CategoryList />
-    </Route>
-    <Route exact path="/:categoryId">
-      <ModelList />
-    </Route>
-    <Route exact path="/:categoryId/:modelId">
-      <ModelDetails />
-    </Route>
-  </Switch>
-)
+const Router: React.FC = () => {
+  const location = useLocation()
+
+  const routeMotion = {
+    initial: 'initial',
+    animate: 'in',
+    exit: 'out',
+    variants: {
+      initial: {
+        x: 20,
+        opacity: 0
+      },
+      in: {
+        x: 0,
+        opacity: 1
+      },
+      out: {
+        x: -20,
+        opacity: 0
+      }
+    },
+    transition: {
+      type: 'tween',
+      ease: 'anticipate',
+      duration: 0.5
+    }
+  } as const
+
+  return (
+    <AnimatePresence exitBeforeEnter initial={false}>
+      <Switch location={location} key={location.pathname}>
+        <Route exact path="/">
+          <RouteContainer {...routeMotion}>
+            <CategoryList />
+          </RouteContainer>
+        </Route>
+        <Route exact path="/:categoryId">
+          <RouteContainer {...routeMotion}>
+            <ModelList />
+          </RouteContainer>
+        </Route>
+        <Route exact path="/:categoryId/:modelId">
+          <RouteContainer {...routeMotion}>
+            <ModelDetails />
+          </RouteContainer>
+        </Route>
+      </Switch>
+    </AnimatePresence>
+  )
+}
 
 export const App: React.FC = () => {
   const { status } = useContent()
