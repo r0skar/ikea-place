67cbf4871f9b56b2bf9cf3ee4054e524af3b7e93
Oskar Rainer <rainer.oskar@gmail.com>
2020-01-26 14:28:08 +0100

    add basic caching

--- src/context/Content.tsx
+++ src/context/Content.tsx
@@ -12,6 +12,7 @@ export enum Status {
   FETCHING = 'FETCHING'
 }
 
+const CACHE = window.sessionStorage
 const MODELS_ENDPOINT = 'http://dev2.viewar.com/api10/models/ids:'
 const CATEGORIES_ENDPOINT = 'http://dev2.viewar.com/api40/tree/channel:3265/version:1579872378'
 const Context = React.createContext({} as State)
@@ -21,11 +22,19 @@ export const useContent = () => {
 }
 
 export const Provider: React.FC = props => {
+  const [status, setStatus] = useState()
   const [data, setData] = useState<Content>([])
-  const [status, setStatus] = useState(Status.FETCHING)
 
   const fetchData = async () => {
+    const cached = CACHE.getItem('content')
+
     setStatus(Status.FETCHING)
+
+    if (cached) {
+      setData((JSON.parse(cached) as unknown) as Content)
+      setStatus(Status.FETCHED)
+      return
+    }
 
     try {
       const categoriesReq = await fetch(CATEGORIES_ENDPOINT)
@@ -37,6 +46,7 @@ export const Provider: React.FC = props => {
 
       setData(content)
       setStatus(Status.FETCHED)
+      CACHE.setItem('content', JSON.stringify(content))
     } catch (e) {
       console.error(e)
       setStatus(Status.FAILED)
