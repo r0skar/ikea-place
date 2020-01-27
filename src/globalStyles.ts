fe4cfe3b5734a03c7e74a633ddecd722a6b15173
Oskar Rainer <rainer.oskar@gmail.com>
2020-01-27 15:39:26 +0100

    refactor root container

--- src/globalStyles.ts
+++ src/globalStyles.ts
@@ -7,6 +7,12 @@ export const GlobalStyle = createGlobalStyle`
 
   html {
     font-size: calc(10px + 0.75vmin);
+    height: 100%;
+    overflow-y: scroll;
+    overflow-x: hidden;
+    overscroll-behavior: none;
+    scroll-behavior: smooth;
+    -webkit-overflow-scrolling: touch;
   }
 
   body {
@@ -15,17 +21,5 @@ export const GlobalStyle = createGlobalStyle`
     font-family: 'Noto Sans', -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, "Noto Sans", sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
     font-size: 1rem;
     line-height: 1.5;
-  }
-
-  #root {
-    /* FIXME: this causes some weird behaviour on iOS Chrome, but works fine everywhere else. */
-    /* Using a fixed height would help, but disables all parallax effects. */
-    min-height: 100vh;
-    position: relative;
-    overflow-y: scroll;
-    overflow-x: hidden;
-    overscroll-behavior: none;
-    scroll-behavior: smooth;
-    -webkit-overflow-scrolling: touch;
   }
 `
